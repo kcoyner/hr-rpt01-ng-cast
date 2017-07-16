2dae14e819b04d07219d827ff716163f0b5f3ebe
Kevin Coyner <rustybear@gmail.com>
2017-07-15 20:04:10 -0400

    kMac.home: Progress capture with videos listing OK from sampleData.

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,15 @@
 angular.module('video-player')
 .component('videoList', {
-  // TODO
+  templateUrl: './src/templates/videoList.html',
+
+  controller: function () {
+    // console.log('this: ', this);
+  },
+
+  bindings: {
+    videos: '<'
+  }
+
+
 });
+
